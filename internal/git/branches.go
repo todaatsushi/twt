b9bd3f868677957aa86ec82bdece66e1cf5f09d6
Atsushi Toda <todaatsushi0@gmail.com>
2023-02-26 14:47:29 +0000

    Git checks for branches

---
+++ internal/git/branches.go
@@ -0,0 +1,24 @@
+package git
+
+import (
+	"fmt"
+
+	"github.com/go-cmd/cmd"
+)
+
+func IsNewBranch(branch string) bool {
+	app := "git"
+	args := []string{"show-ref", fmt.Sprintf("refs/heads/%s", branch)}
+	c := cmd.NewCmd(app, args...)
+	<-c.Start()
+	out := c.Status().Stdout
+	return len(out) > 0
+}
+
+func IsCheckedOut(branch string) bool {
+	grepCmd := fmt.Sprintf("git worktree list | grep %s", branch)
+	c := cmd.NewCmd("bash", "-c", grepCmd)
+	<-c.Start()
+	out := c.Status().Stdout
+	return len(out) > 0
+}
