e2a21e810d0d425a880462527db64b6bd273b817
Atsushi Toda <todaatsushi0@gmail.com>
2023-03-03 21:52:07 +0000

    Expand worktree cmd - cleanup all of git

--- internal/git/worktrees.go
+++ internal/git/worktrees.go
@@ -2,14 +2,16 @@ package git
 
 import "github.com/go-cmd/cmd"
 
-func RemoveWorktree(worktree string, force bool) {
+func RemoveWorktree(name, branch string, force, deleteBranch bool) {
 	app := "git"
-	args := []string{"worktree", "remove", worktree}
-
+	args := []string{"worktree", "remove", name}
 	if force {
 		args = append(args, "--force")
 	}
-
 	c := cmd.NewCmd(app, args...)
 	<-c.Start()
+
+	if deleteBranch {
+		DeleteBranch(branch, force)
+	}
 }
