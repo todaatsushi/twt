18e862eb96f5f655ad0cf058078a734bcd4a7160
Atsushi Toda <todaatsushi0@gmail.com>
2023-03-10 16:35:32 +0000

    Add common file path fetchers

---
+++ internal/utils/commonFiles.go
@@ -0,0 +1,48 @@
+package utils
+
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/todaatsushi/twt/internal/git"
+)
+
+func GetCommonFilesDirPath() (string, error) {
+	baseDir, err := git.GetBaseDir()
+	if err != nil {
+		return "", err
+	}
+
+	path := fmt.Sprintf("%s/common", baseDir)
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return "", errors.New("Common files dir doesn't exist.")
+	}
+	return path, nil
+}
+
+func GetScriptsDirPath() (string, error) {
+	commonFilesDir, err := GetCommonFilesDirPath()
+	if err != nil {
+		return "", err
+	}
+
+	path := fmt.Sprintf("%s/scripts", commonFilesDir)
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return "", errors.New("Scripts dir doesn't exist.")
+	}
+	return path, nil
+}
+
+func GetScriptsDirPathForCommand(command string) (string, error) {
+	scriptsDir, err := GetScriptsDirPath()
+	if err != nil {
+		return "", err
+	}
+
+	path := fmt.Sprintf("%s/%s", scriptsDir, command)
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return "", errors.New("Scripts dir doesn't exist.")
+	}
+	return path, nil
+}
