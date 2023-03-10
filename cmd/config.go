86c59e42e72a0ff953ae6671281feb3fb93ff5e9
Atsushi Toda <todaatsushi0@gmail.com>
2023-03-10 02:22:57 +0000

    Create scripts per new command

--- cmd/config.go
+++ cmd/config.go
@@ -46,6 +46,7 @@ var common = &cobra.Command{
 		path := baseDir
 		toCreate := []string{"common", "scripts"}
 
+		// Create nested dirs
 		for _, dir := range toCreate {
 			path = fmt.Sprintf("%s/%s", path, dir)
 			if _, err := os.Stat(path); !os.IsNotExist(err) {
@@ -55,10 +56,47 @@ var common = &cobra.Command{
 			color.Cyan(fmt.Sprintf("Creating %s.", path))
 
 			if err := os.Mkdir(path, NEW_DIR_PERM); err != nil {
-				color.Red(fmt.Sprintf("Dir %s couldn't be created", dir))
+				color.Red(fmt.Sprintf("Dir %s couldn't be created: %s.", dir, err))
 				return
 			} else {
-				color.Green(fmt.Sprintf("Successfully created %s in %s", dir, path))
+				color.Green(fmt.Sprintf("Successfully created %s in %s.", dir, path))
+			}
+		}
+
+		color.Cyan("\n\nSetting up scripts dirs for each command.\n\n")
+		// Command scripts for each command
+		cmdDirs := []string{"go", "rm"}
+		scripts := []string{"post.sh"}
+		for _, dir := range cmdDirs {
+			newPath := fmt.Sprintf("%s/%s", path, dir)
+
+			if _, err := os.Stat(newPath); !os.IsNotExist(err) {
+				color.Yellow(fmt.Sprintf("Dir %s exists - skipping.", dir))
+				continue
+			}
+			color.Cyan(fmt.Sprintf("Creating %s.", newPath))
+
+			if err := os.Mkdir(newPath, NEW_DIR_PERM); err != nil {
+				color.Red(fmt.Sprintf("Dir %s couldn't be created: %s.", dir, err))
+				return
+			} else {
+				color.Green(fmt.Sprintf("Successfully created %s in %s.", dir, newPath))
+			}
+
+			for _, fileName := range scripts {
+				filePath := fmt.Sprintf("%s/%s", newPath, fileName)
+
+				if _, err := os.Stat(filePath); !os.IsNotExist(err) {
+					color.Yellow(fmt.Sprintf("Dir %s exists - skipping.", dir))
+					continue
+				}
+
+				if _, err := os.Create(filePath); err != nil {
+					color.Red(fmt.Sprintf("File %s couldn't be created: %s.", filePath, err))
+					return
+				} else {
+					color.Green(fmt.Sprintf("Successfully created %s", filePath))
+				}
 			}
 		}
 	},
