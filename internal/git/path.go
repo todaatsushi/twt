4bdd642f9dea5ab992b86341860ffe93def74332
Atsushi Toda <todaatsushi0@gmail.com>
2023-03-13 18:30:42 +0000

    bug: fix base dir getter

--- internal/git/path.go
+++ internal/git/path.go
@@ -2,27 +2,35 @@ package git
 
 import (
 	"errors"
+	"os"
 	"path/filepath"
 
 	"github.com/go-cmd/cmd"
 	"github.com/todaatsushi/twt/internal/checks"
 )
 
-func GetBaseDir() (string, error) {
+func getGitDir() (string, error) {
+	return os.Getwd()
+}
+
+func getBaseFromWorktree() (string, error) {
 	app := "git"
 	args := []string{"rev-parse", "--show-toplevel"}
 	c := cmd.NewCmd(app, args...)
 	<-c.Start()
 	out := c.Status().Stdout
+
 	if len(out) == 0 {
 		return "", errors.New("Couldn't get root git worktree dir - is this a git dir?")
 	}
+	return filepath.Dir(out[0]), nil
+}
 
+func GetBaseDir() (string, error) {
 	if checks.IsInWorktree() {
-		return filepath.Dir(out[0]), nil
+		return getBaseFromWorktree()
+	} else if checks.InGitDir() {
+		return getGitDir()
 	}
-	if checks.InGitDir() {
-		return out[0], nil
-	}
-	return "", errors.New("Couldn't fetch git base dir. Base dir is returned when command is run inside a worktree or the git dir (ie. the base of the worktree).")
+	return "", errors.New("Not in worktree or git dir.")
 }
